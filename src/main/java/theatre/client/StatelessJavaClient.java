a9d92e9c94d60200102e7f0ab40349e00be54054
aymericboccard <aymeric.boccard@etu.emse.fr>
2016-12-16 15:03:53 +0100

    add comments

--- src/main/java/theatre/client/StatelessJavaClient.java
+++ src/main/java/theatre/client/StatelessJavaClient.java
@@ -1,46 +1,6 @@
-/*
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
- *
- * The contents of this file are subject to the terms of either the GNU
- * General Public License Version 2 only ("GPL") or the Common Development
- * and Distribution License("CDDL") (collectively, the "License").  You
- * may not use this file except in compliance with the License.  You can
- * obtain a copy of the License at
- * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
- * or packager/legal/LICENSE.txt.  See the License for the specific
- * language governing permissions and limitations under the License.
- *
- * When distributing the software, include this License Header Notice in each
- * file and include the License file at packager/legal/LICENSE.txt.
- *
- * GPL Classpath Exception:
- * Oracle designates this particular file as subject to the "Classpath"
- * exception as provided by Oracle in the GPL Version 2 section of the License
- * file that accompanied this code.
- *
- * Modifications:
- * If applicable, add the following below the License Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyright [year] [name of copyright owner]"
- *
- * Contributor(s):
- * If you wish your version of this file to be governed by only the CDDL or
- * only the GPL Version 2, indicate your decision by adding "[Contributor]
- * elects to include this software in this distribution under the [CDDL or GPL
- * Version 2] license."  If you don't indicate a single choice of license, a
- * recipient has the option to distribute your version of this file under
- * either the CDDL, the GPL Version 2 or to extend the choice of license to
- * its licensees as provided above.  However, if you add GPL Version 2 code
- * and therefore, elected the GPL Version 2 license, then the option applies
- * only if the new code is made subject to such option by the copyright
- * holder.
- */
+/* interface client */
 
 package theatre.client;
-
-
 import java.util.HashMap;
 import java.util.Scanner;
 import javax.naming.InitialContext;
@@ -51,23 +11,26 @@ public class StatelessJavaClient {
 	public static Scanner sc = new Scanner(System.in);
 
     public static void main(String args[]) {
+    	//menu
     	System.out.println("Bonjour! Bienvenue au theatre!\n Que voulez-vous faire?\n 1. Regarder les événements \n"
     			+ " 2. Faire une reservation \n 3. Regarder mes reservations \n 4. Partir\n\n");
     	String valeur = sc.nextLine();
     	
-    	boolean partir = false;
+    	boolean partir = false; //commande la sortie de l'interface
     	boolean unvalid = (!valeur.equals("1") && !valeur.equals("2") && !valeur.equals("3") && !valeur.equals("4"));
     	
     	
     	while (!partir){
+    		//on ne doit pas afficher la première fois, d'où la condition bizarre
     		if(unvalid ^ (!valeur.equals("1") && !valeur.equals("2") && !valeur.equals("3") && !valeur.equals("4"))){
     			System.out.println("Que voulez-vous faire?\n 1. Regarder les événements \n"
 	        			+ " 2. Faire une reservation \n 3. Regarder mes reservations \n 4. Partir\n\n");
-	        	valeur = sc.nextLine();
+	        	valeur = sc.nextLine(); // doit être doubler certaine foit pour fonctionner correctement
 	        	valeur = sc.nextLine();
 	        	unvalid = (!valeur.equals("1") && !valeur.equals("2") && !valeur.equals("3") && !valeur.equals("4"));
     		}
     		
+    		//permet de foltrer les commandes invalides
 	    	while (unvalid){
 	    		System.out.println("Désolé, action non valide \n Que voulez-vous faire?\n 1. Regarder les événements \n"
 	        			+ " 2. Faire une reservation \n 3. Regarder mes reservations \n 4. Partir\n\n");
@@ -98,7 +61,8 @@ public class StatelessJavaClient {
     	}
         sc.close();
     }
-
+    
+    //montre la liste des événements disponibles
     public static boolean showEvent(){
         try {
 
@@ -114,6 +78,7 @@ public class StatelessJavaClient {
         return askContinue();
     }
     
+    //réservation
     public static boolean booking(){
     	InitialContext ic;
 		try {
@@ -150,6 +115,7 @@ public class StatelessJavaClient {
 	    	}
 	    	
 	    	float price;
+	    	//pour éviter d'écrire 4 fois le même code
 	    	HashMap<String, String> rangeseat = new HashMap<String,String>();
 	    	rangeseat.put("A", "1-25");
 	    	rangeseat.put("B", "1-45");
@@ -173,7 +139,7 @@ public class StatelessJavaClient {
     		 System.out.println("Veuillez renseigner votre prénom suivi de votre nom :");
     		 String username = sc.nextLine();
     		 username = sc.nextLine();
-
+    		 //simulation de paiement
     		 System.out.println("Veuillez renseigner votre numéro de carte bleue :");
     		 String numcard = sc.nextLine();
     		 
@@ -202,6 +168,7 @@ public class StatelessJavaClient {
     	return true;
     }
     
+    //affiche les événements auxquels on participe
     public static boolean showMyEvent(){
     	try {
 
